ba306a530c728e0a016bd9e7b760c1774fda0980
Adrian Koh <koh.adrian.2019@gmail.com>
2025-07-14 17:38:46 +0800

    chore: eslint cleanup

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
-const simpleImportSort = require("eslint-plugin-simple-import-sort");
-const pluginJest = require('eslint-plugin-jest');
+import { defineConfig } from 'eslint/config';
+import expoConfig from 'eslint-config-expo/flat';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import pluginJest, { environments } from 'eslint-plugin-jest';
 
-module.exports = defineConfig([
+export default defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
     plugins: {
-      "simple-import-sort": simpleImportSort,
-      jest: pluginJest
+      'simple-import-sort': simpleImportSort,
+      jest: pluginJest,
     },
     rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-      "import/first": "error",
-      "import/newline-after-import": "error",
-      "import/no-duplicates": "error",
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
       // below rules are from eslint-plugin-jest
       'jest/no-disabled-tests': 'warn',
       'jest/no-focused-tests': 'error',
@@ -30,7 +30,7 @@ module.exports = defineConfig([
     // eslint for jest
     files: ['**/*.spec.js', '**/*.test.js', '**/*.spec.ts', '**/*.test.ts'],
     languageOptions: {
-      globals: pluginJest.environments.globals.globals,
+      globals: environments.globals.globals,
     },
   },
 ]);
